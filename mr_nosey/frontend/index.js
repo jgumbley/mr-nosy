1b7f103491a72b213395bceecd99efda870a05f2
Jim Gumbley <jgumbley@thoughtworks.com>
2015-06-21 16:12:46 +0100

    switch to use circles via join rather than append

--- mr_nosey/frontend/index.js
+++ mr_nosey/frontend/index.js
@@ -52,7 +52,7 @@ var LiteralJSON = React.createClass({
     return (
       <div className="LiteralJSON">
         <hr/>
-        <D3Chart width={1140} height={100} message={this.state.json}/>
+        <D3Chart width={1140} height={400} data={this.state.json}/>
         <hr/>
         <ToggleDebugPanelCheckbox checked={this.state.debug} onUpdate={this.handleUpdateDebugState} />
         { this.state.debug ? (
@@ -74,31 +74,30 @@ var D3Chart = React.createClass({
    componentDidUpdate: function() {
    /* Seems not unreasonable to have the D3 initialise in here
     */
+        console.log(this.props.data.data.children);
         this.updateD3();
     },
    componentDidMount: function() {
    /* Seems not unreasonable to have the D3 initialise in here
     */
+        this.svg = d3.select(this.getDOMNode()).append("svg");
+        this.svg
+            .attr("width", this.props.width)
+            .attr("height", this.props.height);
+        
         this.updateD3();
     },
    updateD3: function() {
-    var svg = d3.select("div.D3Chart").append("svg")
-        .attr("width", this.props.width)
-        .attr("height", this.props.height);
-        svg.style('background-color','red'); 
-        svg.append('rect')
-            .attr('x',10)
-            .attr('y',10)
-            .attr('width', 100)
-            .attr('height', 20);
-        svg.append('text')
-            .classed('book-label','true')
-            .attr('x', 120)
-            .attr('y', 20)
-            .attr('text-anchor', 'start')
-            .attr('fill','black')
-            .text(this.props.message);
-     console.log("hello bim");
+        
+        this.circleJoin = this.svg.selectAll("circle")
+                    .data(["50", "200"]);
+
+        this.circleJoin.enter().append("circle")
+                .attr("r", 30)
+                .attr("cx", 100)
+                .attr("cy", function(d) {return d;})
+                .text("hello");
+        
    }
 });
 
@@ -107,110 +106,4 @@ React.render(
   document.getElementById('content')
 );
 
-
-var width = 960,
-    height = 500,
-    root;
-
-var force = d3.layout.force()
-    .linkDistance(80)
-    .charge(-120)
-    .gravity(.05)
-    .size([width, height])
-    .on("tick", tick);
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-var link = svg.selectAll(".link"),
-    node = svg.selectAll(".node");
-
-d3.json("api", function(error, json) {
-  if (error) throw error;
-
-  root = json;
-  update();
-});
-
-function update() {
-  var nodes = flatten(root),
-      links = d3.layout.tree().links(nodes);
-
-  // Restart the force layout.
-  force
-      .nodes(nodes)
-      .links(links)
-      .start();
-
-  // Update links.
-  link = link.data(links, function(d) { return d.target.id; });
-
-  link.exit().remove();
-
-  link.enter().insert("line", ".node")
-      .attr("class", "link");
-
-  // Update nodes.
-  node = node.data(nodes, function(d) { return d.id; });
-
-  node.exit().remove();
-
-  var nodeEnter = node.enter().append("g")
-      .attr("class", "node")
-      .on("click", click)
-      .call(force.drag);
-
-  nodeEnter.append("circle")
-      .attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; });
-
-  nodeEnter.append("text")
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name; });
-
-  node.select("circle")
-      .style("fill", color);
-}
-
-function tick() {
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
-
-  node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-}
-
-function color(d) {
-  return d._children ? "#3182bd" // collapsed package
-      : d.children ? "#c6dbef" // expanded package
-      : "#fd8d3c"; // leaf node
-}
-
-// Toggle children on click.
-function click(d) {
-  if (d3.event.defaultPrevented) return; // ignore drag
-  if (d.children) {
-    d._children = d.children;
-    d.children = null;
-  } else {
-    d.children = d._children;
-    d._children = null;
-  }
-  update();
-}
-
-// Returns a list of all nodes under the root.
-function flatten(root) {
-  var nodes = [], i = 0;
-
-  function recurse(node) {
-    if (node.children) node.children.forEach(recurse);
-    if (!node.id) node.id = ++i;
-    nodes.push(node);
-  }
-
-  recurse(root);
-  return nodes;
-}
 
