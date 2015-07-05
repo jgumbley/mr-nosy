3998225faa3f2127bd535db0e6ea9753b15d7180
Jim Gumbley <jgumbley@thoughtworks.com>
2015-07-05 20:03:27 +0100

    changes to wifi sniffing stuff

--- wifi/sniff.py
+++ wifi/sniff.py
@@ -1,38 +1,15 @@
 #!/usr/bin/python
 
-from color import *
-from netaddr import EUI
-from scapy.all import *
-
-import re
+from scapy.all import Dot11, Dot11Beacon, sniff
 
 interface = "mon0"
-# see: http://nullege.com/codes/show/src@a@i@airodump-iv-HEAD@airoiv@airodump-iv.py/20/scapy.layers.dot11.Dot11ProbeReq
 
-def ascii_printable(s):
-	return ''.join(i for i in s if ord(i)>31 and ord(i)<128)
+def interpret_packet(packet):
+    if packet.haslayer(Dot11):
+        # print "ap-> %s client-> %s | %s | %s" \
+        #       % (packet.addr1, packet.addr2, packet.addr3, packet.type)
+        if packet.type == 0 and packet.subtype == 8:
+            print "ap! %s ssid-> %s" % (packet.addr2, packet.info)
 
-def get_manuf(m):
-	try:
-		mac = EUI(m)
-		manuf = mac.oui.records[0]['org'].split(' ')[0].replace(',','')
-		#.replace(', Inc','').replace(' Inc.','')
-	except:
-		manuf='unknown'
-	return ascii_printable(manuf)
-
-probeReqs = []
-
-def jims(packet):
-    p = packet
-    if p.haslayer(Dot11):
-        if p.getlayer(Dot11ProbeReq):
-            netName = p.getlayer(Dot11ProbeReq).info
-            if netName not in probeReqs:
-                probeReqs.append(netName)
-                msg= "mac=%s manuf=%s network='%s'" % (p.addr2, get_manuf(p.addr2),
-                                                       netName)
-                print ascii_printable(msg)
-
-sniff(iface='mon0', prn=lambda x:jims(x), store=0)
+sniff(iface='mon0', prn=lambda x:interpret_packet(x), store=0)
 
