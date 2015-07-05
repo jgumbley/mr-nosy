6f284e3674686c2a40c3a2a31c2b716f9ff42497
Jim Gumbley <jgumbley@thoughtworks.com>
2015-07-05 10:38:11 +0100

    trivially returning radio from api

--- mr_nosey/tests/test_webapp.py
+++ mr_nosey/tests/test_webapp.py
@@ -1,12 +1,19 @@
 from hamcrest import assert_that, equal_to
 import unittest
 import webapp
+import json
 
 
 class WebappTestCase(unittest.TestCase):
     def setUp(self):
         self.app = webapp.app.test_client()
 
-    def test_hello_w(self):
-        result = self.app.get("/api")
-        assert_that(result.data, equal_to('{\n  "data": "something"\n}'))
+    def test_add_radio_to_empty(self):
+        # given
+        radio = {"name": "a mac address"}
+
+        # when
+        self.app.post("/api/merge_radio", radio)
+        # then
+        result = self.app.get("/api/all_radios")
+        assert_that(json.loads(result.data), equal_to(radio))
--- mr_nosey/webapp.py
+++ mr_nosey/webapp.py
@@ -14,14 +14,22 @@ def hello():
     data = {"req_id": req_id,
             "name": "steve",
             "children": [
-                {"name": "00:1C:B3:09:85:15", "type": "sta", "ap": "00:1C:B3:09:85:16"},
-                {"name": "00:1C:B3:09:85:16", "type": "ap"},
-                {"name": "00:1C:B3:09:85:17", "type": "sta", "ap": "00:1C:B3:09:85:16"},
-                {"name": str(uuid.uuid1()), "type": "sta", "ap": "00:1C:B3:09:85:16"}
+                {"name": "00:1C:B3:09:85:15",
+                 "type": "sta", "ap": "00:1C:B3:09:85:16"},
+                {"name": "00:1C:B3:09:85:16",
+                 "type": "ap"},
+                {"name": "00:1C:B3:09:85:17",
+                 "type": "sta", "ap": "00:1C:B3:09:85:16"},
+                {"name": str(uuid.uuid1()),
+                 "type": "sta", "ap": "00:1C:B3:09:85:16"}
             ]
             }
     return jsonify({"data": data})
 
+
+@app.route("/api/all_radios")
+def all_radios():
+    return jsonify({"name": "a mac address"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
