1a67515d31243aba749b8cc018ed81a726765e2f
Jim Gumbley <jgumbley@thoughtworks.com>
2015-07-05 11:32:33 +0100

    start of an API around redis

--- mr_nosey/tests/test_webapp.py
+++ mr_nosey/tests/test_webapp.py
@@ -8,12 +8,40 @@ class WebappTestCase(unittest.TestCase):
     def setUp(self):
         self.app = webapp.app.test_client()
 
+    def app_jsonpost(self, uri, data):
+        jsonified = json.dumps(data)
+        self.app.post(uri, data=jsonified,
+                      content_type='application/json')
+
+    def app_allradios(self):
+        result = self.app.get("/api/all_radios")
+        return json.loads(result.data)["data"]
+
+    def app_blankradios(self):
+        self.app.post("/api/blank_radios")
+
+    def test_empty_radios(self):
+        self.app_blankradios()
+        # then
+        assert_that(self.app_allradios(), equal_to([]))
+
     def test_add_radio_to_empty(self):
         # given
-        radio = {"name": "a mac address"}
-
+        self.app_blankradios()
+        radio = {u"name": u"a mac address"}
         # when
-        self.app.post("/api/merge_radio", radio)
+        self.app_jsonpost("/api/merge_radio", radio)
         # then
-        result = self.app.get("/api/all_radios")
-        assert_that(json.loads(result.data), equal_to(radio))
+        assert_that(self.app_allradios(), equal_to([radio]))
+
+    def test_add_another_radio(self):
+        # given
+        radio = {u"name": u"a mac address"}
+        radio2 = {u"name": u"another mac address"}
+        # when
+        self.app_jsonpost("/api/merge_radio", radio)
+        self.app_jsonpost("/api/merge_radio", radio2)
+        # then
+        both_radios = [radio, radio2]
+        assert_that(self.app_allradios(), equal_to(both_radios))
+
--- mr_nosey/webapp.py
+++ mr_nosey/webapp.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+import json
+from flask import Flask, jsonify, request
+from flask_redis import Redis
 import uuid
 
 app = Flask(__name__,
@@ -6,6 +8,9 @@ app = Flask(__name__,
             static_folder='frontend'
             )
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
+redis = Redis(app)
+app.config['REDIS_HOST'] = 'localhost'
+app.config['REDIS_PORT'] = 6379
 
 
 @app.route("/api")
@@ -27,9 +32,38 @@ def hello():
     return jsonify({"data": data})
 
 
-@app.route("/api/all_radios")
+def get_radios():
+    radios = json.loads(redis.get("radios"))
+    if radios is None:
+        radios = []
+    print radios
+    return radios
+
+
+def set_radios(radios):
+    print radios
+    redis.set("radios", json.dumps(radios))
+
+
+@app.route("/api/blank_radios", methods=['POST'])
+def blank_radios():
+    set_radios([])
+    return "hello"
+
+
+@app.route("/api/merge_radio", methods=['POST'])
+def merge_radio():
+    radios = get_radios()
+    radios.append(request.json)
+    set_radios(radios)
+    return "hello"
+
+
+@app.route("/api/all_radios", methods=['GET'])
 def all_radios():
-    return jsonify({"name": "a mac address"})
+    radios = get_radios()
+    print jsonify(data=radios)
+    return jsonify(data=radios)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
