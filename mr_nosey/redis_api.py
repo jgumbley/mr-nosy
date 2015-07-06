48414fb60381dd3f2aae28ec076dbcc369dc4c09
Jim Gumbley <jgumbley@thoughtworks.com>
2015-07-06 09:36:45 +0100

    don't store the name inside of the redis stuff

--- mr_nosey/redis_api.py
+++ mr_nosey/redis_api.py
@@ -12,16 +12,24 @@ class Radio_API(object):
 
     def get_radios(self):
         result = map(
-            lambda radio: json.loads(self.redis.get(radio)),
+            lambda radio: dict(
+                json.loads(self.redis.get(radio)).items() +
+                {"name": radio.replace(self.KEY, "")}.items()
+            ),
             self.redis.scan_iter(match=self.KEY_MATCH)
             )
         return result
 
     def blank_radios(self):
+        """Reasonable to expect this needs to support only
+        removing our keys in the future - extremely bad
+        citizenship
+        """
         self.redis.flushdb()
 
     def merge_radio(self, radio):
         key = self._key_for_radio(radio)
+        del radio["name"]
         self.redis.set(key, json.dumps(radio))
 
     def get_radio(self, key):
